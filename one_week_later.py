33fc89570a1d5f4c193b623d5f1306880092744b
Ray Stallard <rinmynth@gmail.com>
2026-05-31 21:35:52 -0700

    added main function and copied some code in it

--- one_week_later.py
+++ one_week_later.py
@@ -12,45 +12,55 @@ SHORT_MONTH_DAYS = 30 # Apr, Jun, Sep, Nov
 # -----------------------------
 # User Input
 # -----------------------------
-user_month = int(input("Enter a month (1–12): "))
-user_day = int(input("Enter a day (1–31): "))
 
-# Copy input values so we can keep originals for final output
-month = user_month
-day = user_day
 
-# -----------------------------
-# Add 7 days
-# -----------------------------
-day += DAYS_IN_WEEK
+def find_date_one_week_later(month, date):
+    # Copy input values so we can keep originals for final output 
 
-# -----------------------------
-# Adjust for month boundaries
-# -----------------------------
 
-# February (28 days)
-if month == 2 and day > FEB_DAYS:
-    month += 1
-    day -= FEB_DAYS
+    # -----------------------------
+    # Add 7 days
+    # -----------------------------
+    day += DAYS_IN_WEEK
 
-# Months with 31 days
-elif (month == 1 or month == 3 or month == 5 or
-      month == 7 or month == 8 or month == 10 or
-      month == 12) and day > LONG_MONTH_DAYS:
-    month += 1
-    day -= LONG_MONTH_DAYS
+    # -----------------------------
+    # Adjust for month boundaries
+    # -----------------------------
 
-# Months with 30 days
-elif day > SHORT_MONTH_DAYS:
-    month += 1
-    day -= SHORT_MONTH_DAYS
+    # February (28 days)
+    if month == 2 and day > FEB_DAYS:
+        month += 1
+        day -= FEB_DAYS
 
-# Wrap around past December
-if month > 12:
-    month %= 12
+    # Months with 31 days
+    elif (month == 1 or month == 3 or month == 5 or
+        month == 7 or month == 8 or month == 10 or
+        month == 12) and day > LONG_MONTH_DAYS:
+        month += 1
+        day -= LONG_MONTH_DAYS
+
+    # Months with 30 days
+    elif day > SHORT_MONTH_DAYS:
+        month += 1
+        day -= SHORT_MONTH_DAYS
+
+    # Wrap around past December
+    if month > 12:
+        month %= 12
 
 # -----------------------------
 # Output
 # -----------------------------
-print("A week after " + str(user_month) + "/" + str(user_day) +
+def main():
+    user_month = int(input("Enter a month (1–12): "))
+    user_day = int(input("Enter a day (1–31): "))
+    
+    month = user_month
+    day = user_day
+
+
+    print("A week after " + str(user_month) + "/" + str(user_day) +
       " is " + str(month) + "/" + str(day))
+    
+if __name__ == "__name__":
+    main()
